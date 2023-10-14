22cf685549f6ca265549ca26af432770b75ce5e1
Manoka <minashi2004@gmail.com>
2023-10-14 14:17:44 +0300

    First try with SQLServer

---
+++ shalost/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progect1.shalost
+{
+    public class Context : DbContext
+    {
+        public Context() 
+        {
+            Database.EnsureCreated(); //метод, который создает БД        
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=shalost;"); //подключение к SQLServer и создание под него БД
+        }
+
+        public DbSet<User> Users { get; set; } // таблица "Пользователи" из User.cs
+        public DbSet<Role> Roles { get; set; } // таблица "Роли" из Role.cs
+    }
+}
---
+++ shalost/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progect1.shalost
+{
+    public class Role
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
